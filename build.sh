ec6875d5db282cdb6d171eb178ebb9436adbc4e6
areg <areg.nakashian@gmail.com>
2025-06-29 00:14:07 -0400

    Refactor codebase
    Signed-off-by: areg <areg.nakashian@gmail.com>

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "Building..."
-if (g++ main.cpp -o asciipp `pkg-config --cflags --libs opencv4`); then
+if (g++ src/main.cpp -o asciipp `pkg-config --cflags --libs opencv4`); then
     echo "Great success!"
     echo "Usage: ./asciipp <path-of-image> [width | default = 100]"
 else
