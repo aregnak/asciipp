0bd6676a46994a91ba025b8a83102960479ca1bb
areg <areg.nakashian@gmail.com>
2025-06-28 21:56:08 -0400

    V0.1
    working
    
    Signed-off-by: areg <areg.nakashian@gmail.com>

--- art.h
+++ art.h
@@ -8,24 +8,37 @@ class Art
 {
 public:
     Art(cv::Mat image, int outputWidth)
-    : aspectRatio(0.5)
+    : aspectRatio(0.5) // Play with this for different output sizes
     , outputHeight((image.rows * outputWidth * aspectRatio) / image.cols)
     {
+        // Convert to grayscale and resize
         cv::cvtColor(image, image, cv::COLOR_RGB2GRAY);
+        cv::resize(image, image, cv::Size(outputWidth, outputHeight));
 
+        asciiArt.resize(image.rows , std::vector<char>(image.cols));
+
+        // Now using resized rows and cols
         for (int y = 0; y < image.rows; ++y) 
         {
             for (int x = 0; x < image.cols; ++x) 
             {
+                uchar pixel = image.at<uchar>(y, x); // 0-255
+                int index = static_cast<int>((pixel / 255.0) * (chars.size()));
+                asciiArt[y][x] = chars[index]; 
+                if (x == 0)
+                {
+                    std::cout << std::endl << chars[index];
+                }
+                else 
+                {
+                    std::cout << chars[index];
+                }
             }
         }
-
     }
 
-    
-
 private:
-    std::vector<std::vector<char>> asciiart;
+    std::vector<std::vector<char>> asciiArt;
 
     std::string chars = "$@#%/~-. ";
 
