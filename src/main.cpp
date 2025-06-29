6a9dab39973b7e08ac620893155318b8e01ed133
areg <areg.nakashian@gmail.com>
2025-06-29 10:35:56 -0400

    Error handling using throw/catch
    Signed-off-by: areg <areg.nakashian@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,46 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/opencv.hpp>
+#include <stdexcept>
 #include <iostream>
 
 #include "art.h"
 
 int main(int argc, char** argv)
 {
-    if (argc < 2 || argc > 3) 
+    try 
     {
-        std::cerr << "usage: ./asciipp <path-of-image> [width | default = 100]" << std::endl;
-        return -1;
-    }
-
-    // Read image from command-line
-    cv::Mat image = cv::imread(argv[1], 1);
-    if (!image.data) 
-    {
-        std::cerr << "No image data." << std::endl;
-        return -1;
-    }
-
-    if (argc == 3)
-    {
-        int outputWidth = std::stoi(argv[2]);
-        if (outputWidth)
+        if (argc < 2 || argc > 3) 
         {
-            Art art(image, outputWidth);
+            throw std::runtime_error("usage: ./asciipp <path-of-image> [width | default = 100]");
         }
-        else 
+
+        // Read image from command-line
+        cv::Mat image = cv::imread(argv[1], 1);
+        if (!image.data) 
         {
-            std::cerr << "usage: ./asciipp <path-of-image> [width | default = 100]" << std::endl;
-            return -1;
+            std::cerr << "No image data." << std::endl;
+            throw std::runtime_error("Error: No image data.");
         }
-    }
-    else 
-    {
-        Art art(image, 100);
+        
+        // Default output width
+        int outputWidth = 100;
+        if (argc == 3)
+        {
+            if (!std::stoi(argv[2]))
+            {
+                throw std::runtime_error("Error: Width must be a positive integer.");
+            }
+
+            outputWidth = std::stoi(argv[2]);
+        }
+        Art art(image, outputWidth);
+
+        return 0;
     }
 
-    return 0;
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
 }
