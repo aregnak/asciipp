01da1c598e8934a4bc0dd863d92889252c660bc7
areg <areg.nakashian@gmail.com>
2025-07-12 20:21:21 -0400

    Update build message
    Signed-off-by: areg <areg.nakashian@gmail.com>

--- build.sh
+++ build.sh
@@ -5,7 +5,10 @@ set -e
 echo "Building..."
 if (g++ src/main.cpp -o asciipp `pkg-config --cflags --libs opencv4`); then
     echo "Great success!"
-    echo "Usage: ./asciipp <path-of-image> [width | default = 100]"
+    echo -e "\nUsage: ./asciipp <path-of-image>"
+    echo -e "\nOptions:"
+    echo "  -w    Output width, default is 100"
+    echo "  -i    Invert characterset"
 else
     echo "Error while compiling."
 fi
